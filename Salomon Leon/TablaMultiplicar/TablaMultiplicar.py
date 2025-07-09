8a5081a908166ba462a9f4e9fa299178e3747649
Salo-D <salomonleonpesoa74@gmail.com>
2025-07-09 01:19:13 -0400

    Create TablaMultiplicar.py

---
+++ Salomon Leon/TablaMultiplicar/TablaMultiplicar.py
@@ -0,0 +1,9 @@
+print("TABLAS DE MULTIPLICAR")
+print("Introduzca el numero de la tabla:")
+num_tabla=int(input())
+print(f"-----Tabla del {num_tabla}-----")
+for i in range(1,11):
+    resultado = num_tabla * i
+    print(f"{num_tabla} x {i} = {resultado}")
+    
+print("----Fin del progrma-----Salomon Leon")
