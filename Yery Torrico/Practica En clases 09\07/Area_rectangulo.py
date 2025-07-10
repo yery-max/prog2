f84d08a475720e00810218513e89f3ff2c18bcf0
yery-max <yerytr18062003@gmail.com>
2025-07-09 21:07:14 -0400

    Update Area_rectangulo.py

--- Yery Torrico/Practica En clases 09\07/Area_rectangulo.py
+++ Yery Torrico/Practica En clases 09\07/Area_rectangulo.py
@@ -1,1 +1,14 @@
+def Area_rectangulo(base, altura):
+  return base * altura
 
+while True:
+  try:
+    base = float(input("Ingresa la base del rectangulo: "))
+    altura = float(input("Ingresa la altura del rectangulo: "))
+    print(f"El area del rectangulo es: {Area_rectangulo(base, altura)}")
+  except  ValueError:
+    print("Debes ingresar un numero valido")
+    continue
+  continuar = input("¿Desea ingresar otros datos? (Y/N): "). strip().lower()
+  if continuar != 'y':
+    break
