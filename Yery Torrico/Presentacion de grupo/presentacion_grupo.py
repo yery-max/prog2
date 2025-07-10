2c72df63b92d4049c4ae0c18b6510051c63f2f92
yery-max <yerytr18062003@gmail.com>
2025-07-10 19:23:53 -0400

    Update presentacion_grupo.py

--- Yery Torrico/Presentacion de grupo/presentacion_grupo.py
+++ Yery Torrico/Presentacion de grupo/presentacion_grupo.py
@@ -1,1 +1,7 @@
+# Paso 1: Crear una lista con nombres de estudiantes
+nombres_estudiantes = ['Yery', 'Limber', 'Salomon', 'Beymar', 'Daniel', 'Carlos']
+
+# Paso 2 y 3: Usar un bucle for para imprimir un mensaje personalizado
+for nombre in nombres_estudiantes:
+    print(f"¡Bienvenido al equipo los magios, {nombre}!")
 
