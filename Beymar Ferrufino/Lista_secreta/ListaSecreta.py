00d37563c8b0602338e0ede17caee1d1d83422d6
beymar25 <ferrufinobeymar@gmail.com>
2025-07-11 21:10:00 -0400

    Create ListaSecreta.py

---
+++ Beymar Ferrufino/Lista_secreta/ListaSecreta.py
@@ -0,0 +1,11 @@
+# Lista con las notas de los parciales
+notas = [80, 95, 73, 60, 88]
+
+# Calcular el promedio
+suma = sum(notas)
+cantidad = len(notas)
+promedio = suma / cantidad
+
+# Mostrar el resultado
+print("Notas:", notas)
+print("Promedio:", promedio)
