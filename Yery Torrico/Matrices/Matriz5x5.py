142a9b820cdaf86f436e724a76f98586fee5437c
yery-max <yerytr18062003@gmail.com>
2025-07-15 21:04:27 -0400

    Create Matriz5x5.py

---
+++ Yery Torrico/Matrices/Matriz5x5.py
@@ -0,0 +1,9 @@
+matriz =  [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for fila_actual in matriz:
+    for elemento in fila_actual:
+        print(elemento, end=" ")  
+    print()
+print ()
+matriz = [col] + fila * 5 + 1 for col in range(5)] for fila in range(5)]
+for fila_actual in matriz:
+  print ("",join(str(elem) for 
