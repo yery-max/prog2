d6174ab0ad485b77eef7b65befa1b0d41190b8fd
limberdavid <limberdavidquispeosco710@gmail.com>
2025-07-16 23:50:46 +0200

    Update Notas-promedio.py

--- Limber Quispe/Notas Promedio/Notas-promedio.py
+++ Limber Quispe/Notas Promedio/Notas-promedio.py
@@ -1,1 +1,12 @@
+# Lista con las notas de los parciales
+notas = [80, 95, 73, 60, 88]
 
+# Calcular el promedio
+suma = sum(notas)
+cantidad = len(notas)
+promedio = suma / cantidad
+
+# Mostrar el resultado
+print("Notas:", notas)
+print("Promedio:", promedio)
+print("Limber David - FIN DEL PROGRAMA")
