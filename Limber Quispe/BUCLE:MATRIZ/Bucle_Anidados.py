c9fc8228dc80e2401ad5958623ba7df82b1b6181
limberdavid <limberdavidquispeosco710@gmail.com>
2025-07-16 23:43:45 +0200

    Update Bucle_Anidados.py

--- Limber Quispe/BUCLE:MATRIZ/Bucle_Anidados.py
+++ Limber Quispe/BUCLE:MATRIZ/Bucle_Anidados.py
@@ -1,1 +1,14 @@
-
+matriz =[[1,2,3],[4,5,6],[7,8,9]]
+# Opción 1: Recorriendo con índice
+num_filas = len(matriz)
+num_columnas = len(matriz[0])
+for i in range(num_filas):
+    for j in range(num_columnas):
+        elemento = matriz[i][j]
+        print(f"Elemento en la fila {i}, columna {j}: {elemento}")
+# Opción 2: Recorriendo por elemento (mas "pythonico")
+for fila_actual in matriz:
+    for elemento in fila_actual:
+        print(elemento, end=" ")
+    print()
+print("Limber David - FIN DEL PROGRAMA")
