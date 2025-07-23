8e36bfb48cd8508e6ca3ec0433b22bebb30210ed
beymar25 <ferrufinobeymar@gmail.com>
2025-07-22 20:18:10 -0400

    Create Matriz_5.py

---
+++ Beymar Ferrufino/Matriz_5/Matriz_5.py
@@ -0,0 +1,19 @@
+matriz =[[1,2,3],[4,5,6],[7,8,9]]
+for fila_actual in matriz:
+    for elemento in fila_actual:
+        print(elemento, end=" ")
+    print()
+print()
+matriz =[[col + fila * 5+1 for col in range(5)]for fila in range(5)]
+for fila in matriz:
+    print("".join(str(elem) for elem in fila))
+print()
+teclado_numerico =[
+    [1,  2,  3],
+    [4,  5,  6],
+    [7,  8,  9],
+    ["*",0,"#"]
+]
+for fila in teclado_numerico:
+    print("".join(fila))
+print("Beymar Ferrufino - FIN DEL PROGRAMA")
